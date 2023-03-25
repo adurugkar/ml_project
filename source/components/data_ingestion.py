50b5f89ca94bcf6b217c52dcb969d6b48ecf7c7d
avinash <avinashdurugkar69@gmail.com>
2023-03-25 19:33:25 +0530

    changes done

---
+++ source/components/data_ingestion.py
@@ -0,0 +1,59 @@
+from source.logger import logging
+from source.exception import CustomException
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+import os 
+import sys
+import pandas as pd
+from source.components.data_transformation import DataTransformation
+from source.components.data_transformation import DataTransformationConfig
+
+# diffing variable so use data classs
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join('artifact','train.csv')
+    test_data_path = os.path.join('artifact','test.csv') 
+    raw_data_path = os.path.join('artifact','data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig() # ingestion_config storing path of trai,test,raw data
+
+    def initiate_data_ingestion(self):
+        logging.info('Entered the data ingestion method or component')
+        try:
+           df = pd.read_csv('notebook\data\stud.csv')
+           logging.info('read the data set as dataframe')
+
+           #creating folder where we can save the file
+           os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+           # saving the raw data
+           df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+           #splitting the data into train and test
+           logging.info('Train test split initiated')
+           train_set, test_set = train_test_split(df, test_size=0.25, random_state = 42)
+
+           train_set.to_csv(self.ingestion_config.train_data_path, index = False, header=True)
+
+           test_set.to_csv(self.ingestion_config.test_data_path, index = False, header=True)
+
+           logging.info('Train test split completed')
+
+           return(
+               self.ingestion_config.train_data_path,
+               self.ingestion_config.test_data_path
+           )
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__ == '__main__':
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
--- source/exception.py
+++ source/exception.py
@@ -1,12 +1,11 @@
 import sys
-from src.log import logging
+from source.logger import logging 
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno,str(error)
-    )
+        file_name, exc_tb.tb_lineno,str(error))
     return error_message
 
 class CustomException(Exception):
