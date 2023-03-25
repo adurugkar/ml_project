a48803dc0a48a35fb0f924b9a532941436f6a7f2
avinash <avinashdurugkar69@gmail.com>
2023-03-25 19:23:14 +0530

    data Transformation done

--- src/exception.py
+++ src/exception.py
@@ -18,7 +18,7 @@ class CustomException(Exception):
         return self.error_message   
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         a = 1/0
     except Exception as e:
